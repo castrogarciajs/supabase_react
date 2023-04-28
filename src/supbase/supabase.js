4b0468b495331cbf13db8e99573c6a787dd1a818
Sebastian009w <ia3266595@gmail.com>
2023-04-28 15:31:20 -0500

    cliente de supabase

--- src/supbase/supabase.js
+++ src/supbase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supbase_url = process.env.REACT_APP_SUPBASE_PROJECT_URL;
-const supbase_api_key = process.env.REACT_APP_SUPBASE_PROJECT_API_KEY;
-
-createClient({});
+const client = createClient(
+  process.env.REACT_APP_SUPBASE_PROJECT_URL,
+  process.env.REACT_APP_SUPBASE_PROJECT_API_KEY
+);
