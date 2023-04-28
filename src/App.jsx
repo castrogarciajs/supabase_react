e1f1c62440213923dc5c6827759a9200c6840d5c
Sebastian009w <ia3266595@gmail.com>
2023-04-28 16:40:30 -0500

    page no found

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/Login";
 import { HomePage } from "./pages/Home";
+import { NotFoundPage } from "./pages/_404";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
