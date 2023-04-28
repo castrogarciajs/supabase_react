32e364663b38f6e936b123aa2d0c7722964956d7
Sebastian009w <ia3266595@gmail.com>
2023-04-28 15:27:50 -0500

    variables de entorno con react y vite

--- src/supbase/supabase.js
+++ src/supbase/supabase.js
@@ -1,1 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+
+const supbase_url = process.env.SUPBASE_PROJECT_URL;
+const supbase_api_key = process.env.SUPBASE_PROJECT_API_KEY;
+
+createClient({});
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  envDir: {
+    SUPBASE_PROJECT_URL: process.env.SUPBASE_PROJECT_URL,
+    SUPBASE_PROJECT_API_KEY: process.env.SUPBASE_PROJECT_API_KEY,
+  },
+});
