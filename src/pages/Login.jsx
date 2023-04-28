ae210cf9cae4280451242be3cd9a28929a5d28df
Sebastian009w <ia3266595@gmail.com>
2023-04-28 15:52:59 -0500

    evento changes y submit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,21 @@
+import React from "react";
+
 export function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = React.useState("");
+
+  const handleChange = (e) => setEmail(e.target.value);
+
+  const handleSubmit = (e) => {};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" onChange={handleChange} />
+      </p>
+      <p>
+        <button type="submit">Sing In</button>
+      </p>
+    </form>
+  );
 }
