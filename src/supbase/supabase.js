adedd2e87799c167a15dd538c2c3d0d6fb9579b8
Sebastian009w <ia3266595@gmail.com>
2023-04-28 15:38:07 -0500

    correccion en las variables de entorno

--- src/supbase/supabase.js
+++ src/supbase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const client = createClient(
-  process.env.REACT_APP_SUPBASE_PROJECT_URL,
-  process.env.REACT_APP_SUPBASE_PROJECT_API_KEY
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_PROJECT_API_KEY
 );
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: {
-    REACT_APP_SUPBASE_PROJECT_URL: process.env.REACT_APP_SUPBASE_PROJECT_URL,
-    REACT_APP_SUPBASE_PROJECT_API_KEY:
-      process.env.REACT_APP_SUPBASE_PROJECT_API_KEY,
-  },
 });
