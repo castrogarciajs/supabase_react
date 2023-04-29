a21d02f7dc3120b26defdf3d8b32e062a0eb3bd1
Sebastian009w <ia3266595@gmail.com>
2023-04-28 23:42:27 -0500

    manejando el error

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -3,5 +3,9 @@ import { PostContext } from "../context/Context";
 
 export function usePost() {
   const context = useContext(PostContext);
+  if (!context)
+    throw new Error(
+      "para usar el hook usePost debe estar dentro de un contexto"
+    );
   return context;
 }
