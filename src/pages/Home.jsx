5384feceb9e77fb5c8cb018b08268f08f7e473a1
Sebastian009w <ia3266595@gmail.com>
2023-04-28 22:50:55 -0500

    protegiendo todas las rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabase/supabase";
 
 export function HomePage() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    return async () => {
+      const user = await supabase.auth.getUser();
+      if (!user) return navigate("/login");
+    };
+  }, [navigate]);
+
+  const handleLogOut = () => supabase.auth.signOut();
   return (
     <>
       <h1>Home Page</h1>
-      <button>LogOut</button>
+      <button onClick={handleLogOut}>LogOut</button>
     </>
   );
 }
