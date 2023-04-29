f256c015256ad72bca13f18be783d2e8eb9eca61
Sebastian009w <ia3266595@gmail.com>
2023-04-28 22:31:19 -0500

    onAuthState de supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
+import { supabase } from "./supabase/supabase";
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/Login";
 import { HomePage } from "./pages/Home";
 import { NotFoundPage } from "./pages/_404";
 
 function App() {
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      console.log({ event, session });
+    });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
