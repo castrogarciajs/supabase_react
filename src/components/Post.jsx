4fa66dac81e50b94de960367feeecdc3b639dd0d
Sebastian009w <ia3266595@gmail.com>
2023-04-29 15:54:34 -0500

    cambios con el evento delete

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -17,7 +17,7 @@ export function Post({ post }) {
           <p>{JSON.stringify(post.done)}</p>
         </div>
         <div>
-          <button onClick={() => handleDelelte()}>DELETE</button>
+          <button onClick={handleDelelte}>DELETE</button>
           <button onClick={() => handleDone()}>DONE</button>
         </div>
       </div>
