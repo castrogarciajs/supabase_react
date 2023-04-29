4db0a1538ce906923fa058fff8a971ba9583e215
Sebastian009w <ia3266595@gmail.com>
2023-04-29 12:26:38 -0500

    manejadores de evento

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,21 @@
 export function Post({ post }) {
+  const handleDelelte = () => {
+    alert("eliminando");
+  };
+  const handleDone = () => {
+    alert("actulizando");
+  };
   return (
     <>
       <div key={post.id}>
-        <h1>{post.name}</h1>
-        <p>{JSON.stringify(post.done)}</p>
+        <div>
+          <h1>{post.name}</h1>
+          <p>{JSON.stringify(post.done)}</p>
+        </div>
+        <div>
+          <button onClick={() => handleDelelte()}>DELETE</button>
+          <button onClick={() => handleDone()}>DONE</button>
+        </div>
       </div>
     </>
   );
