4b3d774f041de5a5444736c9514a7600e2ba122c
Sebastian009w <ia3266595@gmail.com>
2023-05-02 11:27:54 -0500

    pallete color

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,12 @@
 export function Dashboard({ setDone, done }) {
   return (
     <header className="p-5 flex justify-center gap-20 items-center">
-      <span className="font-bold text-zinc-800">
+      <span className="font-bold text-zinc-800 text-3xl">
         {done ? "Post to do" : "Post done: "}
       </span>
       <button
         onClick={() => setDone(!done)}
-        className="bg-transparent p-2 rounded-md hover:underline transition-colors hover:font-bold"
+        className="bg-transparent p-2 rounded-md hover:underline transition-colors hover:font-bold underline"
       >
         {done ? "Post to do" : "Post done"}
       </button>
