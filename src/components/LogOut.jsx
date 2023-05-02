0c938cf0a869d897551eddc5fa05d25c8894f8d4
Sebastian009w <ia3266595@gmail.com>
2023-05-02 10:26:14 -0500

    styles components

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,8 +1,19 @@
 export function LogOut({ handleLogOut }) {
   return (
-    <header className="bg-green-500">
-      <h2>Home Page</h2>
-      <button onClick={handleLogOut}>LogOut</button>
+    <header className="bg-green-500 w-full max-w-screen-lg py-6 px-8 flex flex-cold md:flex-row gap-4">
+      <div className="flex items-center flex-1">
+        <div className="text-2xl ml-1 font-bold">
+          <h2>HOME PAGE</h2>
+        </div>
+      </div>
+      <div className="flex items-center gap-6">
+        <button
+          onClick={handleLogOut}
+          className="underline font-mono font-bold"
+        >
+          SING OUT
+        </button>
+      </div>
     </header>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,32 +15,29 @@ export function LoginPage() {
   return (
     <div className="flex h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
       <h1 className="md:text-7xl  text-4xl mb-10 font-bold font-mono">
-        Supabase React
+        SUPABASE REACT
       </h1>
-      <form onSubmit={handleSubmit} className="space-y-6 flex flex-col">
+      <form onSubmit={handleSubmit} className="space-y-6 flex flex-col w-96">
         <span className="font-bold text-green-500">{message}</span>
-        <p className="flex flex-col gap-3">
-          <label htmlFor="email" className="font-mono font-bold">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            placeholder="Ingresa tu email"
-            required
-            value={email}
-            className="rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 placeholder:pl-1 outline-none"
-          />
-        </p>
-        <p className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-green-500 p-4 rounded-md w-60 h-10 flex justify-center items-center font-mono font-bold text-white hover:bg-green-700 transition-colors"
-          >
-            Sing In
-          </button>
-        </p>
+        <label htmlFor="email" className="font-mono font-bold">
+          EMAIL
+        </label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          placeholder="Ingresa tu email"
+          required
+          value={email}
+          className="rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 placeholder:pl-1 outline-none placeholder:font-bold"
+        />
+
+        <button
+          type="submit"
+          className="bg-green-500 p-4 rounded-md w-40 h-10 flex justify-center items-center font-mono font-bold text-white hover:bg-green-700 transition-colors"
+        >
+          SING IN
+        </button>
       </form>
     </div>
   );
