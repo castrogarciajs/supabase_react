cd3146a4a1f8289721b0d1e2412cacf4a77a7f28
Sebastian009w <ia3266595@gmail.com>
2023-05-02 13:33:09 -0500

    something wrong

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://cmuvqfkzgpxyijlewkya.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtdXZxZmt6Z3B4eWlqbGV3a3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI3MTIzMjEsImV4cCI6MTk5ODI4ODMyMX0.kyhjFuzzdZYR2ABF_AS4WDkRBSPwaMrwyRgIk4XSHac"
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_PROJECT_API_KEY
 );
