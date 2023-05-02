4e8868203b099d56f6b5b67c34a0eb87a1ce08a6
Sebastian009w <ia3266595@gmail.com>
2023-05-02 12:43:33 -0500

    styles posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,14 +11,26 @@ export function Post({ post }) {
   };
   return (
     <>
-      <div>
-        <div>
-          <h2>{post.name}</h2>
-          <p>{post.done ? "Post completed" : "Post not completed"}</p>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg p-10 pb-5">
+        <div className="px-6 py-4 flex flex-col gap-3 mb-3">
+          <h2 className="text-gray-700 font-bold">{post.name}</h2>
+          <p className={post.done ? "text-green-500" : "text-red-700"}>
+            {post.done ? "Post completed" : "Post not completed"}
+          </p>
         </div>
-        <div>
-          <button onClick={handleDelelte}>DELETE</button>
-          <button onClick={handleDone}>DONE</button>
+        <div className="flex justify-center gap-10 mt-3">
+          <button
+            onClick={handleDelelte}
+            className="bg-transparent hover:bg-red-700 text-gray-600 font-semibold hover:text-white py-2 px-4 border border-red-700 hover:border-transparent rounded"
+          >
+            DELETE
+          </button>
+          <button
+            onClick={handleDone}
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          >
+            DONE
+          </button>
         </div>
       </div>
     </>
